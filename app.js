f0bb197a78e5d260add42b6803a9e17dfd28326c
Aditya Lekhi <adityalekhi88@gmail.com>
2022-01-07 21:15:14 +0530

    Added basic authentication functionality using md5

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const mongoose = require("mongoose");
 const ejs = require('ejs');
 const ejsMate = require("ejs-mate");
 const Menu = require("./models/menu_model");
-const User = require("./models/user_model")
+const User = require("./models/user_model");
+const md5 = require("md5");
+
+let name = "";
+
+// function capture_user(req, res, next) {
+//     user_name = req.body.user_name
+//     next();
+// }
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
 app.set('view engine', "ejs");
 
@@ -15,10 +24,14 @@ mongoose.connect("mongodb://localhost:27017/menu_ejs")
         console.log("Connected to the database");
     })
     .catch((e) => {
-        console.log("Error has occured");
+        console.log("Error has occured in Menu Database");
         console.log(e);
-    })
+    });
 
+
+app.get('/', function (req, res) {
+    res.redirect("menu")
+})
 
 app.get('/menu', function (req, res) {
     Menu.find({}, function (err, results) {
@@ -26,7 +39,7 @@ app.get('/menu', function (req, res) {
             console.log(err);
             res.send("A problem has occured while looking for Menus");
         } else {
-            res.render('menu', { results: results });
+            res.render('menu', { results: results, user_name: name });
         }
     })
 
@@ -36,6 +49,33 @@ app.get('/login', function (req, res) {
     res.render('login');
 })
 
+app.post('/login', function (req, res) {
+    const { user_name, password } = req.body
+    let hash_password = md5(password)
+    name = user_name
+    User.findOne({ Name: user_name }, function (err, found) {
+        if (err) {
+            console.log(err)
+        } else
+            if (found) {
+
+                if (found.Password === hash_password) {
+
+                    res.redirect("menu");
+                } else {
+                    res.redirect("authen")
+                }
+            } else {
+                res.redirect("authen")
+            }
+    })
+})
+
+app.get("/logout", function (req, res) {
+    name = "";
+    res.redirect("authen")
+})
+
 app.get('/authen', function (req, res) {
     res.render('authen');
 })
@@ -43,8 +83,32 @@ app.get('/authen', function (req, res) {
 app.get('/register', function (req, res) {
     res.render('register');
 })
+
+app.post('/register', async function (req, res) {
+    const { user_name, password } = req.body;
+    let hash_password = md5(password)
+    console.log(hash_password);
+    name = user_name
+    const new_user = new User({
+        Name: user_name,
+        Password: hash_password
+    });
+    await new_user.save()
+        .then(() => {
+            console.log("New user has been saved");
+        })
+        .then(() => {
+            res.redirect("menu");
+        })
+
+})
+
 app.get('/submit', function (req, res) {
-    res.render('submit');
+    if (name === "") {
+        res.render('submit_fake');
+    } else {
+        res.render('submit');
+    }
 })
 
 
